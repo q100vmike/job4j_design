bfe998fae6ba0ed9830a329db05a078360bdf04d
q100vmike <melkin@mail.ru>
2024-04-26 16:59:01 +0300

    5. Переопределить только hashCode [#1003]

--- src/main/java/map/User.java
+++ src/main/java/map/User.java
@@ -1,14 +1,23 @@
 package map;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
@@ -18,13 +27,21 @@ public class User {
 
     public static void main(String[] args) {
         Calendar calendar = new GregorianCalendar(2024, Calendar.MARCH, 24);
-
+        calendar.getTime();
         User user1 = new User("Mike", 1, calendar);
         User user2 = new User("Mike", 1, calendar);
 
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
+
+        System.out.println("Год: " + calendar.get(Calendar.YEAR));
+        System.out.println("Месяц: " + calendar.get(Calendar.MONTH));
+        System.out.println("Число: " + calendar.get(Calendar.DAY_OF_MONTH));
+        System.out.println("Часы: " + calendar.get(Calendar.HOUR));
+        System.out.println("Минуты: " + calendar.get(Calendar.MINUTE));
+        System.out.println("Секунды: " + calendar.get(Calendar.SECOND));
+        System.out.println("Миллисекунды: " + calendar.get(Calendar.MILLISECOND));
 
         for (User user : map.keySet()) {
             System.out.println(user);
