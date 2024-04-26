a0f6cf1662742aed5aeb5c77adb95ef567943a8f
q100vmike <melkin@mail.ru>
2024-04-26 17:49:08 +0300

    5. Переопределить только hashCode [#1003]

--- src/main/java/map/User.java
+++ src/main/java/map/User.java
@@ -8,15 +8,8 @@ public class User {
     private Calendar birthday;
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User user = (User) o;
-        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 
     public User(String name, int children, Calendar birthday) {
