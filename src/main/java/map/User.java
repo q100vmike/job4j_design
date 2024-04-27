f4eeeaf44c9a3ab02649e7b1317b4230052a8db5
q100vmike <melkin@mail.ru>
2024-04-27 10:27:25 +0300

    6. Переопределить только equals [#1004]

--- src/main/java/map/User.java
+++ src/main/java/map/User.java
@@ -8,8 +8,15 @@ public class User {
     private Calendar birthday;
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
     }
 
     public User(String name, int children, Calendar birthday) {
