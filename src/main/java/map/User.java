0d01413c87dabafcc31539c5cec12f5872abe6cb
q100vmike <melkin@mail.ru>
2024-04-27 10:44:05 +0300

    7. Переопределить и equals и hashCode [#1002]

--- src/main/java/map/User.java
+++ src/main/java/map/User.java
@@ -19,6 +19,11 @@ public class User {
         return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
