ed630aefef5f87e388b9e96e83f120067a67f340
q100vmike <melkin@mail.ru>
2024-05-23 17:52:25 +0300

    3. Статистика по коллекции. [#45889]

---
+++ src/main/java/question/Analize.java
@@ -0,0 +1,12 @@
+package question;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+    }
+
+}
