7641c00eb68d958ea853f328e447598b9cdc0f72
q100vmike <melkin@mail.ru>
2024-06-19 16:21:14 +0300

    2.1. FileInputStream [#4898]

---
+++ src/main/java/io/EvenNumberFile.java
@@ -0,0 +1,22 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream input = new FileInputStream("data/even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = input.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                System.out.println(Integer.parseInt(line) % 2 == 0 ? "Чет" : "Нечет");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
