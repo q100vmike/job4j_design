1550f98a3d8ce88c93bed9cc6162e24590d73dea
q100vmike <melkin@mail.ru>
2024-06-20 22:12:09 +0300

    5.1. BufferedReader [#252489]

---
+++ src/main/java/io/LogFilter.java
@@ -0,0 +1,33 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+    private final String file;
+
+    public LogFilter(String file) {
+        this.file = file;
+    }
+
+    public List<String> filter() {
+        List<String> result = new ArrayList<String>();
+
+        try (BufferedReader input = new BufferedReader(new FileReader(file))) {
+            input.lines()
+                    .filter(l -> l.substring(l.lastIndexOf(" ") - 3, l.lastIndexOf(" ")).equals("404"))
+                    .forEach(result :: add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter("data/log.txt");
+        logFilter.filter().forEach(System.out::println);
+    }
+}
