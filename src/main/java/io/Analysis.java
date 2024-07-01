07e7c884778bbbc14dd3e6da847269c1224ec0c4
q100vmike <melkin@mail.ru>
2024-07-01 12:41:48 +0300

    7. Анализ доступности сервера [#859 #502939]

--- src/main/java/io/Analysis.java
+++ src/main/java/io/Analysis.java
@@ -8,27 +8,19 @@ public class Analysis {
         StringJoiner output = new StringJoiner("");
 
         try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-            String line = reader.readLine();
-            while (line != null) {
-                if (line.startsWith("400") || line.startsWith("500")) {
-                    if (!start) {
-                        output.add(line.substring(4) + ";");
-                        start = true;
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+                String line = reader.readLine();
+                while (line != null) {
+                    if ((line.startsWith("400") || line.startsWith("500")) && !start) {
+                            writer.write(line.substring(4) + ";");
+                            start = true;
+                    } else if (!(line.startsWith("400") || line.startsWith("500")) && start) {
+                            writer.write(line.substring(4) + ";" + System.lineSeparator());
+                            start = false;
                     }
-                } else {
-                    if (start) {
-                        output.add(line.substring(4) + ";");
-                        output.add(System.lineSeparator());
-                        start = false;
-                    }
+                    line = reader.readLine();
                 }
-                line = reader.readLine();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-            writer.write(output.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
