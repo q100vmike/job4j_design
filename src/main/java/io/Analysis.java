195f1eb4a535ea30a401f20adba2f51128fc5d56
q100vmike <melkin@mail.ru>
2024-07-03 10:23:23 +0300

    7. Анализ доступности сервера [#859 #502939]

--- src/main/java/io/Analysis.java
+++ src/main/java/io/Analysis.java
@@ -5,21 +5,25 @@ import java.util.StringJoiner;
 public class Analysis {
     public void unavailable(String source, String target) {
         boolean start = false;
+        StringBuilder str = new StringBuilder();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
                 String line = reader.readLine();
                 while (line != null) {
                     if ((line.startsWith("400") || line.startsWith("500")) && !start) {
-                            writer.write(line.substring(4) + ";");
+                            str.append(line.substring(4)).append(";");
                             start = true;
                     } else if (!(line.startsWith("400") || line.startsWith("500")) && start) {
-                            writer.write(line.substring(4) + ";" + System.lineSeparator());
+                            str.append(line.substring(4)).append(";").append(System.lineSeparator());
                             start = false;
                     }
                     line = reader.readLine();
                 }
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+            writer.write(String.valueOf(str));
         } catch (IOException e) {
             e.printStackTrace();
         }
