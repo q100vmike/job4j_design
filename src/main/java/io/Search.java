13b33242493947e1cd48ffed6957a7028815766a
q100vmike <melkin@mail.ru>
2024-07-07 14:05:02 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/io/Search.java
+++ src/main/java/io/Search.java
@@ -1,5 +1,6 @@
 package io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,14 +9,29 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        checkArguments(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void checkArguments(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("No Arguments found!");
+        }
+        File file = new File(args[0]);
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException("1'st argument are wrong!");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("2'nd argument are wrong!");
+        }
     }
 }
