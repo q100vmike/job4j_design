d4d5d63eb232febd742f1c3c95b064616887c7df
q100vmike <melkin@mail.ru>
2024-07-19 11:24:00 +0300

    3. Бот [#7921]

--- src/main/java/io/EchoServer.java
+++ src/main/java/io/EchoServer.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
+        StringBuilder message = new StringBuilder();
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -15,9 +16,14 @@ public class EchoServer {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
                         System.out.println(string);
-                        if (string.contains("msg=Bye")) {
-                            server.close();
-                        }
+                        message.append(string);
+                    }
+                    if (message.indexOf("msg=Exit") > 0) {
+                        server.close();
+                    } else if (message.indexOf("msg=Hello") > 0) {
+                        output.write("Hello".getBytes());
+                    } else {
+                        output.write("What".getBytes());
                     }
                     output.flush();
                 }
