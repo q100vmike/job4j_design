dd870f8212eb9761b3b7796eff2f7f662c091992
q100vmike <melkin@mail.ru>
2024-07-22 11:58:55 +0300

    2. Формат JSON [#313164]

--- src/main/java/serialization/json/Film.java
+++ src/main/java/serialization/json/Film.java
@@ -8,19 +8,21 @@ import java.util.List;
 public class Film {
 
     private String name;
+    private boolean hit;
     private List<String> genre;
     private int year;
     private Person starring;
 
-    public Film(String name, List<String> genre, int year, Person starring) {
+    public Film(String name, boolean hit, List<String> genre, int year, Person starring) {
         this.name = name;
         this.genre = genre;
         this.year = year;
         this.starring = starring;
+        this.hit = hit;
     }
 
     public static void main(String[] args) {
-        Film film = new Film("Alien", List.of("Action", "Horror"), 1979,
+        Film film = new Film("Alien", true, List.of("Action", "Horror"), 1979,
                 new Person("Sigourney Weaver", 30));
         final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(film));
