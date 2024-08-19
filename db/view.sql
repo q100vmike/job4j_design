8c6d86f6692cb245e9460b73b4ab61b3d3762501
q100vmike <melkin@mail.ru>
2024-08-19 13:04:59 +0300

    1. Представления [#504792]

---
+++ db/view.sql
@@ -0,0 +1,75 @@
+create table students
+(
+    id   serial primary key,
+    name varchar(50)
+);
+
+insert into students (name)
+values ('Иван Иванов');
+insert into students (name)
+values ('Петр Петров');
+create table authors
+(
+    id   serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name)
+values ('Александр Пушкин');
+insert into authors (name)
+values ('Николай Гоголь');
+
+create table books
+(
+    id serial primary key,
+    name varchar(200),
+    author_id integer references authors (id)
+);
+
+insert into books (name, author_id)
+values ('Евгений Онегин', 1);
+insert into books (name, author_id)
+values ('Капитанская дочка', 1);
+insert into books (name, author_id)
+values ('Дубровский', 1);
+insert into books (name, author_id)
+values ('Мертвые души', 2);
+insert into books (name, author_id)
+values ('Вий', 2);
+
+create table orders
+(
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books (id),
+    student_id integer references students (id)
+);
+
+insert into orders (book_id, student_id)
+values (1, 1);
+insert into orders (book_id, student_id)
+values (3, 1);
+insert into orders (book_id, student_id)
+values (5, 2);
+insert into orders (book_id, student_id)
+values (4, 1);
+insert into orders (book_id, student_id)
+values (2, 2);
+
+create view show_some_students
+as
+select
+    count(s.name) as count
+,s.name as name
+from students as s
+         join orders o on s.id = o.student_id
+         join books b on o.book_id = b.id
+         join authors a on b.author_id = a.id
+where (s.name like '%Иван%' or s.name like '%Петр%')
+and (a.name like '%Пушкин' or a.name like 'Гоголь%')
+and ((b.name like '%дочка%' or b.name like '%души%') and b.name not like '%сын%')
+group by ( s.name)
+having count(s.name) > 0 
+order by s.name desc;
+
+select * from show_some_students;
