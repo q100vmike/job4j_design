65d376767d65e3d0894a7f3f300e47dc766ebd42
q100vmike <melkin@mail.ru>
2025-10-23 12:12:29 +0300

    Вывод бинарного дерева на консоль [#504978]

---
+++ src/main/java/collection/binarytree/PrintTree.java
@@ -0,0 +1,95 @@
+package collection.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintTree {
+
+    private PrintTree() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static String getTreeDisplay(VisualNode root) {
+        StringBuilder buffer = new StringBuilder();
+        List lines = new ArrayList<>();
+        List level = new ArrayList<>();
+        List next = new ArrayList<>();
+        level.add(root);
+        int nodeCount = 1;
+        int widest = 0;
+        while (nodeCount != 0) {
+            nodeCount = 0;
+            List line = new ArrayList<>();
+            for (Object node : level) {
+                if (node == null) {
+                    line.add(null);
+                    next.add(null);
+                    next.add(null);
+                } else {
+                    String key = ((VisualNode) node).getText();
+                    line.add(key);
+                    if (key.length() > widest) {
+                        widest = key.length();
+                    }
+                    next.add(((VisualNode) node).getLeft());
+                    if (((VisualNode) node).getLeft() != null) {
+                        nodeCount++;
+                    }
+                    next.add(((VisualNode) node).getRight());
+                    if (((VisualNode) node).getRight() != null) {
+                        nodeCount++;
+                    }
+                }
+            }
+            if (widest % 2 == 1) {
+                widest++;
+            }
+            lines.add(line);
+            List temp = level;
+            level = next;
+            next = temp;
+            next.clear();
+        }
+        List line = (List) lines.get(lines.size() - 1);
+        int perPiece =  line.size() * (widest + 4);
+        for (int i = 0; i < lines.size(); i++) {
+            line = (List) lines.get(i);
+            int hpw = (int) Math.floor(perPiece / 2f) - 1;
+            if (i > 0) {
+                for (int j = 0; j < line.size(); j++) {
+                    char symbol = ' ';
+                    if (j % 2 == 1) {
+                        if (line.get(j - 1) != null) {
+                            symbol = (line.get(j) != null) ? '\u2534' : '\u256F';
+                        } else if (line.get(j) != null) {
+                            symbol = '\u2570';
+                        }
+                    }
+                    buffer.append(symbol);
+                    if (line.get(j) == null) {
+                        buffer.append(" ".repeat(Math.max(0, perPiece - 1)));
+                    } else {
+                        buffer.append(String.valueOf(j % 2 == 0 ? " " : '\u2500')
+                                .repeat(Math.max(0, hpw)));
+                        buffer.append(j % 2 == 0 ? '\u256D' : '\u256E');
+                        buffer.append(String.valueOf(j % 2 == 0 ? '\u2500' : " ")
+                                .repeat(Math.max(0, hpw)));
+                    }
+                }
+                buffer.append('\n');
+            }
+            for (Object word : line) {
+                if (word == null) {
+                    word = "";
+                }
+                double space = perPiece / 2f - ((String) word).length() / 2f;
+                buffer.append(" ".repeat(Math.max(0, (int) Math.ceil(space))))
+                        .append(word)
+                        .append(" ".repeat(Math.max(0, (int) Math.floor(space))));
+            }
+            buffer.append('\n');
+            perPiece /= 2;
+        }
+        return buffer.toString();
+    }
+}
