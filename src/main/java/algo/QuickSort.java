3fa521b39af1ce5b26149f02dfa3f3d73bdb4005
q100vmike <melkin@mail.ru>
2025-12-10 17:49:39 +0300

    QuickSort

--- src/main/java/algo/QuickSort.java
+++ src/main/java/algo/QuickSort.java
@@ -1,7 +1,30 @@
 package algo;
 
 public class QuickSort {
-    public static void main(String[] args) {
 
+    public static int partition(int[] arr) {
+        int pivot = arr[0];
+        int beginToEnd = pivot + 1;
+        int endToBegin = arr.length - 1;
+
+            while (arr[beginToEnd] < pivot) {
+                beginToEnd++;
+            }
+            while (arr[endToBegin] > pivot) {
+                endToBegin--;
+            }
+        if (beginToEnd >= endToBegin) {
+
+        }
+
+    }
+
+    public static void sort(int[] arr, int left, int right) {
+        if (left < right) {}
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 8, 2, 7, 3, 4, 6, 9, 12};
+        int i = partition(arr);
     }
 }
