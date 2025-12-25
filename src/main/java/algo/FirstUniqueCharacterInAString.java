da2ad54e03f313ad21888a623083dbc0eb10e13a
q100vmike <melkin@mail.ru>
2025-12-25 17:51:55 +0300

    алгоритмы тест

--- src/main/java/algo/FirstUniqueCharacterInAString.java
+++ src/main/java/algo/FirstUniqueCharacterInAString.java
@@ -1,4 +1,5 @@
 package algo;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,7 +22,7 @@ public class FirstUniqueCharacterInAString {
        return -1;
     }
 
-    public int firstUniqChar(String s) {
+    public int firstUniqCharOn2(String s) {
         int frq  = 0;
         int i = 0;
 
@@ -45,6 +46,39 @@ public class FirstUniqueCharacterInAString {
 
         return frq == 1 ? i - 1 : -1;
     }
+
+    public int firstUniqChar(String s) {
+
+        int[][] array = new int[122][2];
+        int max = Integer.MAX_VALUE;
+
+        for (int i = 0; i < s.length(); i++) {
+            //char c = s.charAt(i);
+            int ascii = (int) s.charAt(i);
+            array[ascii][0] = i;
+            array[ascii][1] += 1;
+//            if (array[ascii][0] == 0) {
+//                array[ascii][0] = i + 1;
+//            } else {
+//                array[ascii][0] = max;
+//            }
+        }
+
+        int[][] newMas = Arrays.copyOfRange(array, 97, 124);
+        //Arrays.sort(newMas, (row1, row2) -> Integer.compare(row1[1], row2[1]));
+
+        for (int j = 0; j < newMas.length; j++) {
+            int a = newMas[j][0];
+            int b = newMas[j][1];
+
+            if (newMas[j][1] == 1) {
+                return newMas[j][0];
+            }
+        }
+
+        return -1;
+    }
+
 }
 
 
